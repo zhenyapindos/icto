9fe650e29b4d2a5ee852706aa9dddfad7eff7a2b
zhenyapindos <89606930+zhenyapindos@users.noreply.github.com>
2023-12-05 18:54:48 +0300

    Add files via upload

--- week06/pentagon.js
+++ week06/pentagon.js
@@ -23,20 +23,17 @@ AFRAME.registerComponent('run', {
     this.B = document.querySelector("#B");
     this.C = document.querySelector("#C");
     this.D = document.querySelector("#D");
-    this.E = document.querySelector("#F");
 
     this.text = document.querySelector("#P");
     this.perimeterText = document.querySelector("#perimeterText");
     this.areaText1 = document.querySelector("#areaText1");
     this.areaText2 = document.querySelector("#areaText2");
     this.areaText3 = document.querySelector("#areaText3");
-    this.areaText4 = document.querySelector("#areaText4");
 
     this.pA = new THREE.Vector3();
     this.pB = new THREE.Vector3();
     this.pC = new THREE.Vector3();
     this.pD = new THREE.Vector3();
-    this.pE = new THREE.Vector3();
 
     let material = new THREE.MeshLambertMaterial({ color: 0xFF0000 });
     let geometry = new THREE.CylinderGeometry(0.05, 0.05, 1, 12);
@@ -49,30 +46,24 @@ AFRAME.registerComponent('run', {
     this.cylinderAB.visible = false;
 
     this.cylinderBC = new THREE.Mesh(geometry, material);
-    this.lineBC = document.querySelector('#lineBF').object3D;
+    this.lineBC = document.querySelector('#lineBC').object3D;
     this.lineBC.add(this.cylinderBC);
     this.cylinderBC.visible = false;
 
     this.cylinderCD = new THREE.Mesh(geometry, material);
-    this.lineCD = document.querySelector('#lineFD').object3D;
+    this.lineCD = document.querySelector('#lineCD').object3D;
     this.lineCD.add(this.cylinderCD);
     this.cylinderCD.visible = false;
 
-    this.cylinderDE = new THREE.Mesh(geometry, material);
-    this.lineDE = document.querySelector('#lineDC').object3D;
-    this.lineDE.add(this.cylinderDE);
-    this.cylinderDE.visible = false;
-
-    this.cylinderEA = new THREE.Mesh(geometry, material);
-    this.lineEA = document.querySelector('#lineCA').object3D;
-    this.lineEA.add(this.cylinderEA);
-    this.cylinderEA.visible = false;
+    this.cylinderDA = new THREE.Mesh(geometry, material);
+    this.lineDA = document.querySelector('#lineDA').object3D;
+    this.lineDA.add(this.cylinderDA);
+    this.cylinderDA.visible = false;
 
     this.lineAB = this.createLine();
-    this.lineBF = this.createLine();
-    this.lineFD = this.createLine();
-    this.lineDC = this.createLine();
-    this.lineCA = this.createLine();
+    this.lineBC = this.createLine();
+    this.lineCD = this.createLine();
+    this.lineDA = this.createLine();
   },
 
   createLine: function () {
@@ -89,37 +80,46 @@ AFRAME.registerComponent('run', {
       this.B.object3D.getWorldPosition(this.pB);
       this.C.object3D.getWorldPosition(this.pC);
       this.D.object3D.getWorldPosition(this.pD);
-  
+
       // Calculate distances
       let distanceAB = this.pA.distanceTo(this.pB);
       let distanceBC = this.pB.distanceTo(this.pC);
       let distanceCD = this.pC.distanceTo(this.pD);
       let distanceDA = this.pD.distanceTo(this.pA);
-  
+
       // Calculate perimeter
       let perimeter = distanceAB + distanceBC + distanceCD + distanceDA;
-  
+
       // Calculate areas of triangles
       let areaABC = calculateTriangleArea(this.pA, this.pB, this.pC);
       let areaBCD = calculateTriangleArea(this.pB, this.pC, this.pD);
       let areaCDA = calculateTriangleArea(this.pC, this.pD, this.pA);
-  
+
       // Update text entities
       this.perimeterText.setAttribute('text', 'value: Perimeter: ' + perimeter.toFixed(2));
       this.areaText1.setAttribute('text', 'value: Area (ABC): ' + areaABC.toFixed(2));
       this.areaText2.setAttribute('text', 'value: Area (BCD): ' + areaBCD.toFixed(2));
       this.areaText3.setAttribute('text', 'value: Area (CDA): ' + areaCDA.toFixed(2));
-  
+
       // Update cylinder scales and visibility
       this.cylinderAB.scale.set(1, 1, distanceAB);
       this.cylinderBC.scale.set(1, 1, distanceBC);
       this.cylinderCD.scale.set(1, 1, distanceCD);
-      this.cylinderEA.scale.set(1, 1, distanceDA);
-  
-      this.cylinderAB.visible = this.cylinderBC.visible = this.cylinderCD.visible = this.cylinderEA.visible = true;
+      this.cylinderDA.scale.set(1, 1, distanceDA);
+
+      this.cylinderAB.visible = this.cylinderBC.visible = this.cylinderCD.visible = this.cylinderDA.visible = true;
     } else {
       // Hide cylinders if markers are not visible
-      this.cylinderAB.visible = this.cylinderBC.visible = this.cylinderCD.visible = this.cylinderEA.visible = false;
+      this.cylinderAB.visible = this.cylinderBC.visible = this.cylinderCD.visible = this.cylinderDA.visible = false;
     }
   }
 });
+
+function calculateTriangleArea(point1, point2, point3) {
+  // Use Heron's formula to calculate the area of a triangle
+  let a = point1.distanceTo(point2);
+  let b = point2.distanceTo(point3);
+  let c = point3.distanceTo(point1);
+  let s = (a + b + c) / 2;
+  return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+}
